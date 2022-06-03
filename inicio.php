c7d29367ae58106c454b030fdcdd92991a94ce80
garbf1996 <garbfbatista@gmail.com>
2022-06-02 20:43:18 -0400

    creacion de panel de control

--- inicio.php
+++ inicio.php
@@ -73,7 +73,105 @@ include "conexion.php";
          </a>
       </div>
     </div>
-  </section>  
+
+  <div class="">
+    <div class="divContainer">
+      <div>
+        <h1 class="titlepanelcontrol">Configuracion</h1>
+      </div>
+      <div class="containerPerfil">
+        <div class="containerDataUser">
+         <div class="logoUser">
+           <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" width="100">
+         </div>
+         <div class="divDataUser">
+          <h4>Informaacion personal</h4>
+
+          <div>
+            <label>Nombre:</label> <span>Garber</span>
+          </div>
+
+          <div>
+            <label>Correo:</label> <span>garbf@gamil.com</span>
+          </div>
+             
+         <h4>Datos Usuario</h4>
+         <div>
+            <label>Rol:</label> <span>Admin</span>
+          </div>
+
+          <div>
+            <label>Usuario:</label> <span>Garber</span>
+          </div>
+          <h4>Cambiar contraseña</h4>
+          <form method="post" action="" name="frmChangepassword" id="frmChangepassword">
+          <div>
+           <input type="password" name="txtpasswordUser" id="txtpasswordUser" placeholder="Contraseña actual" value="" required>
+            </div>
+              <br>
+            <div>
+             <input type="password" name="txtpasswordnew" id="txtpasswordnew" placeholder="Nuevo contraseña" value="" required>
+            </div>
+            <br>
+            <div>
+              <button type="submit" name="tbn_save" id="tbn_save"><i class="fa fa-key"></i>Cambiar Contraseña</button>
+             </div>
+
+          </form>
+         </div>
+        </div>
+        <div class="containerDataEmpresa">
+          <div class="logoEmpresa">
+          <img src="https://cdn-icons.flaticon.com/png/512/3328/premium/3328269.png?token=exp=1654212693~hmac=14ac051f08d644d252d15b7003759887" width = "100">
+          </div>
+          <h4>Datos del la empresa</h4>
+          <form action="" method="post" name="frmEmpresa" id="frmEmpresa">
+            <input type="hidden" name="action" value="updateDataEmpresa" />
+
+            <div>
+              <label>Nit:</label> <input type="text" name="txtNit" id="txtNit" placeholder="Nit de la empresa" value="" required>
+            </div>
+
+            <div>
+              <label>Nombre:</label> <input type="text" name="txtNombre" id="txtNombre" placeholder="Nombre de la empresa" value="" required>
+            </div>
+               
+            <div>
+              <label>Razon social:</label> <input type="text" name="txtRazon" id="txtRazon" placeholder="Razon social" value="" required>
+            </div>
+
+            <div>
+              <label>Telefono:</label> <input type="text" name="txtTelefono" id="txttelefono" placeholder="number de telefono" value="" required>
+            </div>
+
+            <div>
+              <label>Correo electronico:</label> <input type="email" name="txtemail" id="txtemal" placeholder="Correo electronico" value="" required>
+            </div>
+
+            <div>
+              <label>Dirrecion:</label> <input type="text" name="txtdirrecion" id="txtdirrecion" placeholder="Dirrecion" value="" required>
+            </div>
+
+            <div>
+              <label>ITB%:</label> <input type="text" name="txtIBT" id="txtIBT" placeholder="ITB" value="" required>
+            </div>
+
+            <div class="alertFormEmrpresa" style="display: none"></div>
+             
+            <div>
+              <button type="submit" class="btn_save btnChangepass">
+                <i class="fa fa-save"></i>Guardar datos
+              </button>
+            </div>
+          </form> 
+        </div>
+      </div>
+    </div>
+</div>
+
+  </section> 
+
+
     <script type="text/javascript" src="funcio.js"></script> 
     <script src="jquery/jquery-3.6.0.min.js"></script>	 	
     <script src="popper/popper.min.js"></script>	 	 	
